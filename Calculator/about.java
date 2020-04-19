8a5abfb2b569864ed4d10a9ff3c7922416c89901
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-04-19 04:06:24 -0700

    Create about.java

---
+++ Calculator/about.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+public class about
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void about() throws IOException, InterruptedException
+    {
+        pageone po = new pageone();
+        Exit exit = new Exit();
+        Start srt = new Start();
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        BufferedReader br = new BufferedReader(new FileReader("about.txt"));
+        for (String line;(line = br.readLine()) !=null;)
+        {
+            System.out.println(line);
+        }
+        br.close();
+        System.out.println("\nPress back to go back");
+        String a = sc.next();
+        if (a.equals("back")||a.equals("Back")||a.equals("BACK"))
+        {
+            po.one();
+        }
+        else if (a.equals("0"))
+        {
+            exit.nexit();
+        }
+        else if (a.equals("*"))
+        {
+            srt.main();
+        }
+        else 
+        {
+            System.out.println("Please enter the instructions given to you by the admin.\nenter go to continue");
+            String abc = sc.next();
+            if (abc.equals("go")||abc.equals("GO")||abc.equals("Go")||abc.equals("gO"))
+            {
+                about();
+            }
+            else 
+            {
+                System.out.println("As you are not following the instructions you have been kicked out of the program"); System.exit(0);            
+            }
+        }
+    }
+}
