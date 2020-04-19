9c43ed8cf8697b463d1d37183880280250b4c785
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-04-19 04:07:02 -0700

    Create Number_system.java

---
+++ Calculator/Number_system.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Number_system
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void octal()
+    {
+        System.out.println("Octal");
+    }
+}
