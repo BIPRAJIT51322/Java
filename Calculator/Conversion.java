2f31952f4fb37357e26f7c02a118154ae0d06ac3
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-04-19 04:06:47 -0700

    Create Conversion.java

---
+++ Calculator/Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Conversion
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void binary()
+    {
+        System.out.println("convert");
+    }
+}
