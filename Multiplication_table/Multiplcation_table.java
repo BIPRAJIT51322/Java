dd2efe9391f9e6988a05620cd25cc75b8e24a2bc
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-04-19 04:08:14 -0700

    Create Multiplcation_table.java

---
+++ Multiplication_table/Multiplcation_table.java
@@ -0,0 +1,10 @@
+public class Multiplcation_table
+{
+    public static void main(double number)
+    {
+        for(int a = 1;a<=10;a++)
+        {
+            System.out.println(number+" x "+a+" = "+(number*a));
+        }
+    }
+}
