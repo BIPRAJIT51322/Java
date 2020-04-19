1e2d523ccf528106b3a8012fc66b28570a302168
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-04-19 04:06:59 -0700

    Create Multiplication_table.java

---
+++ Calculator/Multiplication_table.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Multiplication_table
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void decimal()
+    {
+        System.out.println("Decimal");
+    }
+}
