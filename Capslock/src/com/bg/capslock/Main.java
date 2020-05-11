bea30fe019812323d36300f6a72fd85049da58ec
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-05-10 22:52:22 -0700

    Create Main.java

---
+++ Capslock/src/com/bg/capslock/Main.java
@@ -0,0 +1,10 @@
+package com.bg.capslock;
+import java.util.*;
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the word: ");
+        String a = sc.next();
+        System.out.println(a.toUpperCase());
+    }
+}
